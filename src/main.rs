92337628c66bc2f35263d5f069facdda92df3bfb
Matthew Russell <matthewjohnrussell@gmail.com>
2022-05-18 01:06:11 +0100

    use ? instead of try!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-extern crate igd;
-extern crate get_if_addrs;
-
 use std::process::{Command, Stdio, exit};
 use std::io::{self, Write};
-use std::error::Error;
 use std::{env, net};
 
 fn addr_ip(addr: net::Ipv4Addr) -> u32 {
@@ -17,7 +13,7 @@ fn addr_ip(addr: net::Ipv4Addr) -> u32 {
 fn if_for_addr(remote_addr: net::Ipv4Addr) -> io::Result<get_if_addrs::Ifv4Addr> {
     let remote_addr = addr_ip(remote_addr);
 
-    for addr in try!(get_if_addrs::get_if_addrs()) {
+    for addr in get_if_addrs::get_if_addrs()? {
         match addr.addr {
             get_if_addrs::IfAddr::V4(addr) => {
                 let ip = addr_ip(addr.ip);
@@ -45,37 +41,34 @@ fn try_main() -> io::Result<i32> {
         command.arg(arg);
     }
 
-    let output = try!(command.output());
+    let output = command.output()?;
 
     if !output.status.success() {
         return output.status.code()
             .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "child exited by signal"))
     }
 
-    let data = try!(String::from_utf8(output.stdout).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e)));
+    let data = String::from_utf8(output.stdout).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
 
-    let data = try!(data.lines()
+    let data = data.lines()
         .find(|line| line.starts_with("MOSH CONNECT "))
-        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "expected connection data"))
-    );
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "expected connection data"))?;
 
     let mut data = data.splitn(4, ' ').skip(2);
     let data = data.next().and_then(|port| data.next().map(|secret| (port, secret)));
-    let (port, secret) = try!(data.ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "malformated connection data")));
+    let (port, secret) = data.ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "malformated connection data"))?;
 
-    let port = try!(port.parse().map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "connection port must be numeric")));
+    let port = port.parse().map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "connection port must be numeric"))?;
 
-    let gateway = try!(igd::search_gateway()
-        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
-    );
+    let gateway = igd::search_gateway()
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
 
-    let address = try!(if_for_addr(*gateway.addr.ip()));
+    let address = if_for_addr(*gateway.addr.ip())?;
     let address = net::SocketAddrV4::new(address.ip, port);
-    let external_port = try!(gateway.add_any_port(igd::PortMappingProtocol::UDP, address, 0, "mosh-server")
-        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
-    );
+    let external_port = gateway.add_any_port(igd::PortMappingProtocol::UDP, address, 0, "mosh-server")
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
 
-    try!(writeln!(io::stdout(), "\nMOSH CONNECT {} {}", external_port, secret));
+    writeln!(io::stdout(), "\nMOSH CONNECT {} {}", external_port, secret)?;
 
     Ok(0)
 }
@@ -83,7 +76,7 @@ fn try_main() -> io::Result<i32> {
 fn main() {
     match try_main() {
         Err(err) => {
-            let _ = writeln!(io::stderr(), "mosh-server-upnp error: {}", err.description());
+            let _ = writeln!(io::stderr(), "mosh-server-upnp error: {}", err);
             exit(1)
         },
         Ok(status) => exit(status),
